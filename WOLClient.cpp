e0436b052d861d1f1557ce7539de36dc45c48b71
smulewsk <smulewsk@gmail.com>
2023-01-31 12:23:24 +0100

    Add defines for magic numbers

--- WOLClient.cpp
+++ WOLClient.cpp
@@ -5,6 +5,9 @@ using namespace WOL;
 void WOLClient::WakeMe(String^ hwAddr) 
 {
     const auto BUFFER_LENGTH(102);
+    const auto SUBMASK_FILTER(0x00FFFFFF);
+    const auto BROADCAST_ADDRESS_VALUE(0xFF000000);
+    const auto BROADCAST_PORT_VALUE(0x2FFF);
 
     IPHostEntry^ localIpAddressList = Dns::GetHostEntry(Dns::GetHostName());
 
@@ -16,9 +19,9 @@ void WOLClient::WakeMe(String^ hwAddr)
         {
             long long ipAddr = localIpAddressList->AddressList[i]->Address;
             //
-            long long ipAddrBroadcast = (ipAddr & 0x00FFFFFF) | 0xFF000000;
+            long long ipAddrBroadcast = (ipAddr & SUBMASK_FILTER) | BROADCAST_ADDRESS_VALUE;
             //
-            this->Connect(gcnew IPAddress(ipAddrBroadcast), 0x2fff);
+            this->Connect(gcnew IPAddress(ipAddrBroadcast), BROADCAST_PORT_VALUE);
             this->SetClientToBrodcastMode();
 
             //buffer to be send
@@ -43,24 +46,28 @@ void WOLClient::SetClientToBrodcastMode()
 void WOLClient::PrepareMagicPacket(array <unsigned char>^ buffer, String^ hwAddr)
 {
     auto counter(0);
-    
+    const auto BUFFER_HEADING_SIZE(6);
+    const auto MAC_ADDRESS_SIZE(6);
+    const auto MAC_ADDRESS_REPETITIONS(16);
+    const auto HEX_NUMBER_SIZE(2);
+
     // Magic Packet buffer = 6*0xFF + 16*MAC address
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < BUFFER_HEADING_SIZE; i++)
         buffer[counter++] = 0xFF;
 
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < MAC_ADDRESS_REPETITIONS; i++)
     {
         int j = 0;
-        for (int z = 0; z < 6; z++)
+        for (int z = 0; z < MAC_ADDRESS_SIZE; z++)
         {
             if (hwAddr[j] == ':') {
                 j++;
             }
             buffer[counter++] =
-                Byte::Parse(hwAddr->Substring(j, 2),
+                Byte::Parse(hwAddr->Substring(j, HEX_NUMBER_SIZE),
                     NumberStyles::HexNumber);
-            j += 2;
+            j += HEX_NUMBER_SIZE;
         }
     }
 }
